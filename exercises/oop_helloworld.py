5a4a34d7cdc3b7ef905874e5be2104d592421e27
Sarvani Kunapareddy <skunap2@uic.edu>
2022-05-09 13:10:54 -0500

    added car class

--- exercises/oop_helloworld.py
+++ exercises/oop_helloworld.py
@@ -1,4 +1,17 @@
 # create your class here
+class Car: 
+    #instance attributes
+    def __init__(self,model,speed):
+        self.model = model
+        self.speed = speed
+
+    # gets the model
+    def getModel(self):
+        return self.model
+    # sets the speed
+    def setSpeed(self,theSpeed):
+        self.speed = theSpeed
+
 
 if __name__ == '__main__':
     bmw = Car('BMW', 30.3)
