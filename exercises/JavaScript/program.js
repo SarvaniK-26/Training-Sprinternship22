213d8e71ac804b6ca2f8af20a255c830264a82bf
Sarvani Kunapareddy <skunap2@uic.edu>
2022-05-11 09:29:48 -0500

    changed website

--- exercises/JavaScript/program.js
+++ exercises/JavaScript/program.js
@@ -1,10 +1,18 @@
 var body;
 
 // implement onload function
+window.onload = function(){
+    body = document.querySelector("body");
+}
 
 
 // implement changeColorBlue
-
+window.changeColorBlue = function(){
+    body.style.backgroundColor = "#a8ceff"
+}
 
 // implement changeColorPurple
+window.changeColorPurple = function(){
+    body.style.backgroundColor = "#6a0dad "
+}
 
